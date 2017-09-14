c0d51dea93f719b275f029b7df03327160834321
David Arteaga <david@Davids-MacBook-Pro.local>
2017-09-14 16:06:28 -0500

    check price every second, print date next to current price

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const player = require('play-sound')({});
 
-const timeout_ms = 500;
+const timeout_ms = 1 * 1000;
 const url = 'https://cex.io/api/ticker/ETH/USD';
 const alarmFile = './alarm.mp3';
 
@@ -24,7 +24,7 @@ setInterval(() => {
     if (!error) {
       const res = JSON.parse(body);
       const last = parseFloat(res.last);
-      console.log(last);
+      console.log(date(parseFloat(res.timestamp)) + ': ' + last);
       if (last <= minPrice) {
         if (!playing) {
           playing = true;
@@ -38,3 +38,16 @@ setInterval(() => {
     }
   });
 }, timeout_ms);
+
+function date(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+}
