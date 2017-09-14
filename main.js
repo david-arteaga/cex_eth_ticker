4799b6e37eafe53772bb11f35ff065c6730846c8
david-arteaga <daap99@gmail.com>
2017-09-14 18:06:18 -0500

    save eth price to file eth_prices.txt

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const request = require('request');
 const player = require('play-sound')({});
 
+const fs = require('fs');
+const pricesFile = './eth_price.txt';
+
 // cex has a hard limit on 600 requests per 10 minutes (1 request per minute)
 const timeout_ms = 1 * 1000;
 const url = 'https://cex.io/api/ticker/ETH/USD';
@@ -8,7 +11,7 @@ const alarmFile = './alarm.mp3';
 
 if (process.argv.length < 3) {
   console.error('needs 1 input param: price');
-  process.exit(1); 
+  process.exit(1);
 }
 
 let minPrice = parseFloat(process.argv[2]);
@@ -27,6 +30,9 @@ setInterval(() => {
         const res = JSON.parse(body);
         const last = parseFloat(res.last);
         console.log(date(parseFloat(res.timestamp)) + ': ' + last);
+
+        const to_write = res.timestamp + ',' + last + '\n';
+        fs.appendFileSync(pricesFile, to_write);
         if (last <= minPrice) {
           if (!playing) {
             playing = true;
